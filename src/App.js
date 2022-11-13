9ac6e6f1bbc38557eb08450c1fb1ef4ce9f1768f
BrianBett005 <brianbett316@gmail.com>
2022-11-13 08:12:41 +0300

    added frontend code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+// import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import LandingPage from "./pages/LandingPage";
+
+import EditSchool from "./pages/EditUser";
+
+import AddUserDetails from "./pages/AddUserDetails";
+import CreateEvent from "./pages/CreateBlog";
+import Blogs from "./pages/Blogs";
+
+import OverView from "./pages/OverVew";
+import Management from "./pages/Management";
+import Logout from "./pages/Logout";
+import Schools from "./pages/Schools";
+import SingleBlog from "./pages/SingleBlog";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details" element={<AddUserDetails />} />
+        <Route path="/forgot_password" element={<ForgotPassword />} />
+        <Route path="/reset_password" element={<ResetPassword />} />
+        <Route path="/landing_page" element={<LandingPage />} />
+        <Route path="/" element={<OverView />} />
+        <Route path="/edit_profile" element={<EditSchool />} />
+        <Route path="/management" element={<Management />} />
+        <Route path="/create_blog" element={<CreateEvent />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:blog_id" element={<SingleBlog />} />
+        <Route path="/schools" element={<Schools />} />
+      </Routes>
+    </Router>
   );
 }
 
